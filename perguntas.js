7a1c8e8c7c21910b68d479d4c43887669f06f59d
CAUAPENNA <cauapennanasc@gmail.com>
2024-11-29 22:56:47 -0300

    AluraQuest-Nayara

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Biologia',
+    'O que é unha?',
+    'é uma proteção dos dedos'
+)
+
+criaCartao(
+    'Matemática',
+    'Quanto é 1+1?',
+    '2'
+)
+
+criaCartao(
+    'Química',
+    'Qual a formação da água?',
+    'H2O'
+)
+
+criaCartao(
+    'Empreendedorismo',
+    'Como juntar dinheiro?',
+    'Gaste com moderação e sabedoria'
+)
